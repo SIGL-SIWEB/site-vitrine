6c2e235e94187147859563fb791ab1b037e37e43
Pierre Chojka <pchojka@gmail.com>
2017-06-16 16:09:46 +0200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {Routes} from './app.routes';
 
 import { HomepageComponent } from './homepage.component';
 import {NavbarComponent} from './navbar/navbar.component';
+import {ModuleComponent} from './modules/modules.component';
 
 @NgModule({
   declarations: [
     HomepageComponent,
-    NavbarComponent
+    NavbarComponent,
+    ModuleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Routes
   ],
   providers: [],
   bootstrap: [HomepageComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import {RouterModule} from '@angular/router';
+import { ModuleComponent } from './modules/modules.component';
+
+export const Routes = RouterModule.forRoot([
+
+    {
+        path: 'modules',
+        component : ModuleComponent
+    }
+])
+
