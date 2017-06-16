bab3fb509360238e4e6de04b61ab10446abe0dd2
Pierre Chojka <pchojka@gmail.com>
2017-06-16 16:10:19 +0200

    Add content (modules)

---
+++ src/app/modules/modules.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'modules',
+    templateUrl: 'modules.component.html'
+})
+
+export class ModuleComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
