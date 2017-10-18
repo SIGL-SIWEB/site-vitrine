d706d8be057cbf444f807e1ecb5f9a51d54fb1c5
Romain Labat <labat_r@epita.fr>
2017-10-18 14:58:22 +0200

    add token to github preprod url

--- src/app/services/issues.service.ts
+++ src/app/services/issues.service.ts
@@ -9,14 +9,16 @@ import 'rxjs/add/operator/toPromise';
 export class IssueService {
     constructor(private http: Http) { }
 
-    private urlPreprod : string = "https://api.github.com/repos/dev-sigl/site-vitrine/issues"
+    private urlPreprod : string = "https://api.github.com/repos/dev-sigl/site-vitrine/issues";
+    //private urlPreprod : string = "https://api.github.com/repos/dev-sigl/site-vitrine/issues/?access_token=c7b70bc7b7d01ec65969a55367feb2e967d2e2fd";
+    
 
     reportIssue(title : string, body : string) {
         var request = {
             title : title,
             body : body
+           // token : "c7b70bc7b7d01ec65969a55367feb2e967d2e2fd"
         };
-
         return this.http.post(this.urlPreprod, request).toPromise().then(response =>
             console.log(response));
     }
