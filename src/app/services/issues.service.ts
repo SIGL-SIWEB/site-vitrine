c6e5c24457311c07f4a166dbb9cb590f16755f8d
Pierre Chojka <pchojka@gmail.com>
2017-11-04 14:37:14 +0100

    Resolve css bug in english

--- src/app/services/issues.service.ts
+++ src/app/services/issues.service.ts
@@ -9,16 +9,17 @@ import 'rxjs/add/operator/toPromise';
 export class IssueService {
     constructor(private http: Http) { }
 
-    private urlPreprod : string = "https://api.github.com/repos/dev-sigl/site-vitrine/issues";
-    //private urlPreprod : string = "https://api.github.com/repos/dev-sigl/site-vitrine/issues/?access_token=c7b70bc7b7d01ec65969a55367feb2e967d2e2fd";
+   // private urlPreprod : string = "https://api.github.com/repos/dev-sigl/site-vitrine/issues";
+    private urlPreprod : string = "https://api.github.com/repos/dev-sigl/site-vitrine/issues/?access_token=4d5cf6787b2307eef1402420332d151eef9a3765";
     
 
     reportIssue(title : string, body : string) {
         var request = {
             title : title,
             body : body
-           // token : "c7b70bc7b7d01ec65969a55367feb2e967d2e2fd"
         };
+
+        console.log(request);
         return this.http.post(this.urlPreprod, request).toPromise().then(response =>
             console.log(response));
     }
