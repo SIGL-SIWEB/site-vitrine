e20b0872ca60289ada455ad477c184b8631570cb
Baptiste Saint-André <baptiste@mkdmpartner.com>
2019-11-13 23:25:50 +0100

    Styling navbar

--- src/app/homepage.component.ts
+++ src/app/homepage.component.ts
@@ -1,33 +1,33 @@
 import { IssueService } from './services/issues.service';
 import { Component, OnInit } from '@angular/core';
-import * as  $  from 'jquery';
-import {TranslateService} from 'ng2-translate';
+import * as  $ from 'jquery';
+import { TranslateService } from 'ng2-translate';
 
 const defaultLanguage = "en";
-const additionalLanguages = ["fr","en"];
-const languages: string[] = 
-[defaultLanguage].concat(additionalLanguages);
+const additionalLanguages = ["fr", "en"];
+const languages: string[] =
+	[defaultLanguage].concat(additionalLanguages);
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './homepage.component.html',
-  styleUrls : ['./homepage.component.css'],
-  providers : [IssueService]
+	selector: 'app-root',
+	templateUrl: './homepage.component.html',
+	styleUrls: ['./homepage.component.css'],
+	providers: [IssueService]
 })
 export class HomepageComponent implements OnInit {
-		constructor(private translate: TranslateService,
-					private issueServ : IssueService) {
-			this.translate.setDefaultLang(defaultLanguage);
-        this.translate.addLangs(additionalLanguages);
- 
-        let initLang = this.translate.getBrowserLang();
-        if (languages.indexOf(initLang) === -1) {
-            initLang = defaultLanguage;
-        }
-        this.translate.use(initLang);
+	constructor(private translate: TranslateService,
+		private issueServ: IssueService) {
+		this.translate.setDefaultLang(defaultLanguage);
+		this.translate.addLangs(additionalLanguages);
+
+		let initLang = this.translate.getBrowserLang();
+		if (languages.indexOf(initLang) === -1) {
+			initLang = defaultLanguage;
+		}
+		this.translate.use(initLang);
 	}
 
-	private issueClicked : boolean;
+	private issueClicked: boolean;
 
 	ngOnInit() {
 		this.issueClicked = false;
@@ -36,81 +36,80 @@ export class HomepageComponent implements OnInit {
 	clickIssue() {
 		this.issueClicked = !this.issueClicked;
 	}
-	
-	reportIssue(title : string, body : string) {
+
+	reportIssue(title: string, body: string) {
 		return this.issueServ.reportIssue(title, body);
 	}
- 
-   
 
-  changeLanguage(lang){
-	this.translate.use(lang);
-  }
 
-  function ($) {
+	changeLanguage(lang) {
+		this.translate.use(lang);
+	}
 
-	//Typed
-	$(".typed").typed({
-		strings: ["with an amazing design", "easy to customize"],
-		typeSpeed: 100,
-		backDelay: 900,
-		loop: true
-	});
+	function($) {
 
-	//Screenshots
-	$('.screens').owlCarousel({
-		stagePadding: 10,
-		margin: 30,
-		loop: true,
-		responsive: {
-			0: {
-				items: 1
-			},
-			480: {
-				items: 1
-			},
-			768: {
-				items: 2
-			},
-			979: {
-				items: 4
-			}
-		}
-	});
-
-	//Popup video
-	$('#play-video').click(function (e) {
-		e.preventDefault();
-
-		var video_link = $(this).data('video');
-		video_link = '<iframe src="' + video_link + '" width="500" height="208" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-
-		$('.work-video').append(video_link).fadeIn(200);
-	});
-
-	$('.close-video').click(function (e) {
-		e.preventDefault();
-
-		$('.work-video').fadeOut(200, function () {
-			$('iframe', this).remove();
+		//Typed
+		$(".typed").typed({
+			strings: ["with an amazing design", "easy to customize"],
+			typeSpeed: 100,
+			backDelay: 900,
+			loop: true
 		});
 
-	});
+		//Screenshots
+		$('.screens').owlCarousel({
+			stagePadding: 10,
+			margin: 30,
+			loop: true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				480: {
+					items: 1
+				},
+				768: {
+					items: 2
+				},
+				979: {
+					items: 4
+				}
+			}
+		});
 
-};
+		//Popup video
+		$('#play-video').click(function (e) {
+			e.preventDefault();
 
-	ngAfterViewInit () {
-            !function(d,s,id){
-                var js: any,
-                    fjs=d.getElementsByTagName(s)[0],
-                    p='https';
-                if(!d.getElementById(id)){
-                    js=d.createElement(s);
-                    js.id=id;
-                    js.src=p+"://platform.twitter.com/widgets.js";
-                    fjs.parentNode.insertBefore(js,fjs);
-                }
-            }
-            (document,"script","twitter-wjs");
-    }
+			var video_link = $(this).data('video');
+			video_link = '<iframe src="' + video_link + '" width="500" height="208" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+
+			$('.work-video').append(video_link).fadeIn(200);
+		});
+
+		$('.close-video').click(function (e) {
+			e.preventDefault();
+
+			$('.work-video').fadeOut(200, function () {
+				$('iframe', this).remove();
+			});
+
+		});
+
+	}
+
+	ngAfterViewInit() {
+		!function (d, s, id) {
+			var js: any,
+				fjs = d.getElementsByTagName(s)[0],
+				p = 'https';
+			if (!d.getElementById(id)) {
+				js = d.createElement(s);
+				js.id = id;
+				js.src = p + "://platform.twitter.com/widgets.js";
+				fjs.parentNode.insertBefore(js, fjs);
+			}
+		}
+			(document, "script", "twitter-wjs");
+	}
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,33 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import {TranslateService} from 'ng2-translate';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { TranslateService } from 'ng2-translate';
+import * as  $ from 'jquery';
 
 const defaultLanguage = "en";
-const additionalLanguages = ["fr","en"];
-const languages: string[] = 
-[defaultLanguage].concat(additionalLanguages);
+const additionalLanguages = ["fr", "en"];
+const languages: string[] =
+  [defaultLanguage].concat(additionalLanguages);
 
 @Component({
-    selector: 'navbar',
-    templateUrl: 'navbar.component.html'
+  selector: 'navbar',
+  templateUrl: 'navbar.component.html'
 })
 
 
 export class NavbarComponent implements OnInit {
-    constructor(private translate: TranslateService) {
-			this.translate.setDefaultLang(defaultLanguage);
-        this.translate.addLangs(additionalLanguages);
- 
-        let initLang = this.translate.getBrowserLang();
-        if (languages.indexOf(initLang) === -1) {
-            initLang = defaultLanguage;
-        }
-        this.translate.use(initLang);
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang(defaultLanguage);
+    this.translate.addLangs(additionalLanguages);
+
+    let initLang = this.translate.getBrowserLang();
+    if (languages.indexOf(initLang) === -1) {
+      initLang = defaultLanguage;
     }
-
-
-  changeLanguage(lang){
-	this.translate.use(lang);
+    this.translate.use(initLang);
   }
 
-    ngOnInit() { }
+
+
+  @HostListener('window:scroll', ['$event'])
+  scrollHandler(event) {
+    var y = window.scrollY;
+    if (y >= 600) {
+      $("#logo_min").css("display", "block")
+    }
+    else {
+      $("#logo_min").css("display", "none")
+    }
+  }
+
+  changeLanguage(lang) {
+    this.translate.use(lang);
+  }
+
+  ngOnInit() { }
 }
